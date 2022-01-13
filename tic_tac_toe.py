18ecf4d86639e32e88af95f99cf3e13e3d493fd4
mrecancun <mrecancun@hotmail.com>
2022-01-12 18:01:27 -0700

    Game testing

---
+++ tic_tac_toe.py
@@ -0,0 +1,21 @@
+import random
+
+
+def create_board (): 
+    for i in range (3):
+        row = []
+    
+        for i in range (3):
+        row.append ('-')
+
+
+    condition = True
+    if condition:
+        x= 1
+    else condition:
+    x= 0               
+
+    print (x)    
+
+def winner_player ():
+    
